91571af1f5d099b66619b737264784baa97a52f3
Saad Bin Khalid <saadbinkhalid1895@gmail.com>
2026-01-04 16:10:41 +0500

    Set up the project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 bg-primary"
+    >
+      <Text className="text-5xl text-primary font-bold text-center">Welcome To My App!</Text>
+    </View>
+  );
+}
